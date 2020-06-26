44bad737c2f3f6a8d1e83a8a2c0e92e86c424bfc
hi <hi@DESKTOP-G463TQN>
2020-06-26 18:07:09 +0900

    test

--- src/com/javaex/dao/PhoneTest.java
+++ src/com/javaex/dao/PhoneTest.java
@@ -10,18 +10,18 @@ public class PhoneTest {
 		
 		PhoneDao phoneDao = new PhoneDao();
 		
-		/*
+		
 		List<PersonVo> personList = phoneDao.getPersonList();
 		System.out.println(personList.toString());
-		*/
+		
 		
 		/*
 		PersonVo personVo = new PersonVo("유재석", "010-1111-1111", "02-1111-1111");
 		phoneDao.personInsert(personVo);
 		*/
 		
-		PersonVo personVo = phoneDao.getPerson(7);
-		System.out.println(personVo.toString());
+//		PersonVo personVo = phoneDao.getPerson(7);
+//		System.out.println(personVo.toString());
 
 	}
 
